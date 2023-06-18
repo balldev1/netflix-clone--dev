1b2a7df048ee090fabe1fa1695bb98145788a4ff
balldev1 <132042116+balldev1@users.noreply.github.com>
2023-06-18 10:34:53 +0700

    Update favorite.ts

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { without } from "lodash";
 
-import prismadb from  '@/lib/prismadb'
+import prismadb from '@/lib/prismadb';
 import serverAuth from "@/lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
